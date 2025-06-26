38aacf3e4c47ce8e1158049860af686202e3a96e
TI JONNY <ti.jonny@ipti.org.br>
2025-06-26 15:36:05 -0300

    teste pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,20 @@ import http from "@/service/axios";
 
 export default async function Home() {
 
-  const pop = await http.get("/product");
+  // const pop = await http.get("/product");
 
   return (
     <div className="p-4">
       <div className="grid">
-        {pop?.data?.map((item, index) => {
+        {/* {pop?.data?.map((item, index) => {
           return (
             <div className="col-12 md:col-4" key={index} >
               <CardProduct item={item} />
             </div>
           )
-        })}
+        })} */}
+
+        pagina inicial
       </div>
     </div>
 
