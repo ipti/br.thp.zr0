5cb4905fb5e3e4ddd2d43878dfc5ec043d04365e
TI JONNY <ti.jonny@ipti.org.br>
2025-09-01 17:55:11 -0300

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Header from "@/components/header/header";
-import http from "@/service/axios";
 import About from "./components/about/about";
 import Impact from "./components/impact/impact";
 import Product from "./components/product/product";
-import SplitterHome from "./components/splitter_home/splitter_home";
 import VideoComponet from "./components/video/video";
 import Footer from "./components/footer/footer";
 import Gallery from "./components/gallery/gallery";
+import SplitterHome from "./components/splitter_home/splitter_home";
 
 
 
