1a2f36e96448a939f75f10e92b152a1b7ec6c42a
TI JONNY <ti.jonny@ipti.org.br>
2025-09-09 14:50:03 -0300

    / static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,37 @@ import Footer from "./components/footer/footer";
 import Gallery from "./components/gallery/gallery";
 import SplitterHome from "./components/splitter_home/splitter_home";
 
-
+export const revalidate = 60; 
+// a cada 60s revalida, mas serve página estática no meio tempo (ISR)
+// ou use "false" para estático 100% no build
 
 export default async function Home() {
-
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}product`, {
-    cache: "no-store", // se não quiser cache
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product`, {
+    next: { revalidate: 60 }, // tira cache: "no-store"
   });
 
-  console.log(res)
-
-  // if (!res.ok) {
-  //   throw new Error("Erro ao buscar produtos");
-  // }
+  if (!res.ok) {
+    throw new Error("Erro ao buscar produtos");
+  }
 
   const product = await res.json();
 
   return (
     <div>
       <Header />
-      <SplitterHome
-      />
+      <SplitterHome />
       <About />
-
       <VideoComponet />
       <Impact />
       <div className="p-4">
-        {product?.data?.map((item, index) => {
-          return (
-            <div key={index} >
-              <Product item={item} />
-            </div>
-          )
-        })}
+        {product?.data?.map((item: any, index: number) => (
+          <div key={index}>
+            <Product item={item} />
+          </div>
+        ))}
       </div>
       <Gallery />
       <Footer />
     </div>
-
   );
 }
