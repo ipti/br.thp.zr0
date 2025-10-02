b2f76e9bd66bd431ee2b06309d6f25012207392b
TI JONNY <ti.jonny@ipti.org.br>
2025-10-02 15:10:12 -0300

    fix: input number button group

--- src/components/input_number/input_number.tsx
+++ src/components/input_number/input_number.tsx
@@ -1,5 +1,10 @@
 import { InputNumber, InputNumberProps } from "primereact/inputnumber";
+import './input_number.css';
 
 export default function ZInputNumber(props: InputNumberProps) {
-    return <InputNumber {...props} />
+    return <InputNumber {...props} pt={{
+        buttonGroup: {
+            className: 'p-button-group'
+        },
+    }} />
 }
