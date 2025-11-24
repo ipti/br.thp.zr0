805ebba2c28b806c3058fc4399bf34440fd92949
Jonny Walker Paulino de Menezes <jonny_menezes@Mac-mini-de-Jonny.local>
2025-11-24 16:45:49 -0300

    verify env

--- src/app/payment/components/payment.tsx
+++ src/app/payment/components/payment.tsx
@@ -4,6 +4,7 @@ import CheckoutComponent from "@/components/payment/payment";
 import { useParams, useSearchParams } from "next/navigation";
 import { useFetchRequestOrderOne, useFetchRequestPaymentIntentOne } from "../service/query";
 import { OrderOneType } from "../service/types";
+import './payment.css'
 
 export default function PaymentComponent() {
 
@@ -21,54 +22,65 @@ export default function PaymentComponent() {
     );
     
     return (
-        <div className="grid">
-            {order && <div className="col-6 p-4">
+        <div className="grid payment-container  ">
+          {order && (
+                <div className="card">
 
-                <section className="">
-                    <h3>Itens do Pedido ({totalProducts})</h3>
-                    <div className="my-4">
-                        {order.order_items.map((item: any) => (
-                            <div key={item.id} className="flex flex-row">
-                                <div className="item-info">
-                                    <strong>{item.product.name}</strong>
+                    {/* Itens */}
+                    <section>
+                        <h3>Itens do Pedido ({totalProducts})</h3>
+
+                        <div>
+                            {order.order_items.map((item: any) => (
+                                <div key={item.id} className="item-box">
+                                    <div>
+                                        <strong className="product-name">{item.product.name}</strong>
+                                        <p className="sub">Qtd: {item.quantity}</p>
+                                    </div>
+
+                                    <div className="right">
+                                        <p>Preço Unitário: <strong>R$ {item.unit_price.toFixed(2)}</strong></p>
+                                        <p>Total: <strong>R$ {item.total_price.toFixed(2)}</strong></p>
+                                    </div>
                                 </div>
-                                <div className="item-details">
-                                    <p>Qtd: {item.quantity}</p>
-                                    <p>Preço Unitário: R$ {item.unit_price.toFixed(2)}</p>
-                                    <p>Total: R$ {item.total_price.toFixed(2)}</p>
-                                </div>
+                            ))}
+                        </div>
+                    </section>
+
+                    {/* Delivery */}
+                    <section className="delivery-section">
+                        <h3>Entrega</h3>
+
+                        {delivery ? (
+                            <div className="delivery-box">
+                                {delivery.map((item) => (
+                                    <div key={item.id}>
+                                        <p><strong>Transportadora:</strong> {item.delivery_estimate.carrier}</p>
+                                        <p><strong>Serviço:</strong> {item.delivery_estimate.service}</p>
+                                        <p><strong>Prazo:</strong> {item.delivery_estimate.deliveryTime} dias</p>
+                                        <p><strong>Custo:</strong> R$ {item.delivery_estimate.cost.toFixed(2)}</p>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                </section>
+                        ) : (
+                            <p>Sem informações de entrega</p>
+                        )}
+                    </section>
 
-                <section className="order-section delivery">
-                    <h3>Entrega</h3>
-                    {delivery ? <>
-                        {delivery?.map((item) => {
-
-                            return <>
-                                <p><strong>Transportadora:</strong> {item.delivery_estimate.carrier}</p>
-                                <p><strong>Serviço:</strong> {item.delivery_estimate.service}</p>
-                                <p><strong>Prazo:</strong> {item.delivery_estimate.deliveryTime} dias</p>
-                                <p><strong>Custo:</strong> R$ {item.delivery_estimate.cost.toFixed(2)}</p>
-                            </>
-                        }
-                        )}</> : (
-                        <p>Sem informações de entrega</p>
-                    )}
-                </section>
-
-                <footer className="order-footer">
-                    <p><strong>Total do Pedido:</strong> R$ {(order.total_amount + (delivery?.reduce((sum, item) => sum + item.delivery_estimate.cost,
-                        0) ?? 0)).toFixed(2)}</p>
-
-                    <div className="flex flex-row justify-content-end mt-4">
-
-                    </div>
-                </footer>
-            </div>}
-            {order && <div className="col-6"><CheckoutComponent clientSecret={paymentService?.client_secret ?? undefined}/></div>}
+                    {/* Total */}
+                    <footer className="total-box">
+                        <p>
+                            Total do Pedido:
+                            <span className="total-value">
+                                R$ {(order.total_amount +
+                                    (delivery?.reduce((sum, item) => sum + item.delivery_estimate.cost, 0) ?? 0)).toFixed(2)}
+                            </span>
+                        </p>
+                    </footer>
+                </div>
+            )}
+ 
+            {order && <div><CheckoutComponent clientSecret={paymentService?.client_secret ?? undefined}/></div>}
         </div>
     )
 }
